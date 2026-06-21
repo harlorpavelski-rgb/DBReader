16d07f3909642bdaf12e6244f398edb785529d5c
harlorpavelski-rgb <harlorpavelski@gmail.com>
2026-06-21 17:10:02 +0800

    更正同名博文高亮错误问题

---
+++ menuData.js
@@ -0,0 +1,260 @@
+var thispost = [
+    {
+        "category": "标签一",
+        "posts": [
+            {
+                "title": "4_博文",
+                "total_pages": 8,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签一/4_博文/1.html"
+                    },
+                    {
+                        "title": "2",
+                        "path": "./标签一/4_博文/2.html"
+                    },
+                    {
+                        "title": "3",
+                        "path": "./标签一/4_博文/3.html"
+                    },
+                    {
+                        "title": "4",
+                        "path": "./标签一/4_博文/4.html"
+                    },
+                    {
+                        "title": "5",
+                        "path": "./标签一/4_博文/5.html"
+                    },
+                    {
+                        "title": "6",
+                        "path": "./标签一/4_博文/6.html"
+                    },
+                    {
+                        "title": "7",
+                        "path": "./标签一/4_博文/7.html"
+                    },
+                    {
+                        "title": "8",
+                        "path": "./标签一/4_博文/8.html"
+                    }
+                ]
+            },
+            {
+                "title": "5_博文",
+                "total_pages": 1,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签一/5_博文/1.html"
+                    }
+                ]
+            },
+            {
+                "title": "同名博文",
+                "total_pages": 8,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签一/同名博文/1.html"
+                    },
+                    {
+                        "title": "2",
+                        "path": "./标签一/同名博文/2.html"
+                    },
+                    {
+                        "title": "3",
+                        "path": "./标签一/同名博文/3.html"
+                    },
+                    {
+                        "title": "4",
+                        "path": "./标签一/同名博文/4.html"
+                    },
+                    {
+                        "title": "5",
+                        "path": "./标签一/同名博文/5.html"
+                    },
+                    {
+                        "title": "6",
+                        "path": "./标签一/同名博文/6.html"
+                    },
+                    {
+                        "title": "7",
+                        "path": "./标签一/同名博文/7.html"
+                    },
+                    {
+                        "title": "8",
+                        "path": "./标签一/同名博文/8.html"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "category": "标签三",
+        "posts": [
+            {
+                "title": "同名博文",
+                "total_pages": 3,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签三/同名博文/1.html"
+                    },
+                    {
+                        "title": "2",
+                        "path": "./标签三/同名博文/2.html"
+                    },
+                    {
+                        "title": "3",
+                        "path": "./标签三/同名博文/3.html"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "category": "标签二",
+        "posts": [
+            {
+                "title": "1_博文",
+                "total_pages": 8,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签二/1_博文/1.html"
+                    },
+                    {
+                        "title": "2",
+                        "path": "./标签二/1_博文/2.html"
+                    },
+                    {
+                        "title": "3",
+                        "path": "./标签二/1_博文/3.html"
+                    },
+                    {
+                        "title": "4",
+                        "path": "./标签二/1_博文/4.html"
+                    },
+                    {
+                        "title": "5",
+                        "path": "./标签二/1_博文/5.html"
+                    },
+                    {
+                        "title": "6",
+                        "path": "./标签二/1_博文/6.html"
+                    },
+                    {
+                        "title": "7",
+                        "path": "./标签二/1_博文/7.html"
+                    },
+                    {
+                        "title": "8",
+                        "path": "./标签二/1_博文/8.html"
+                    }
+                ]
+            },
+            {
+                "title": "2_博文",
+                "total_pages": 8,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签二/2_博文/1.html"
+                    },
+                    {
+                        "title": "2",
+                        "path": "./标签二/2_博文/2.html"
+                    },
+                    {
+                        "title": "3",
+                        "path": "./标签二/2_博文/3.html"
+                    },
+                    {
+                        "title": "4",
+                        "path": "./标签二/2_博文/4.html"
+                    },
+                    {
+                        "title": "5",
+                        "path": "./标签二/2_博文/5.html"
+                    },
+                    {
+                        "title": "6",
+                        "path": "./标签二/2_博文/6.html"
+                    },
+                    {
+                        "title": "7",
+                        "path": "./标签二/2_博文/7.html"
+                    },
+                    {
+                        "title": "8",
+                        "path": "./标签二/2_博文/8.html"
+                    }
+                ]
+            },
+            {
+                "title": "3_博文",
+                "total_pages": 1,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./标签二/3_博文/1.html"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "category": "其他",
+        "posts": [
+            {
+                "title": "1_无标签博文",
+                "total_pages": 1,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./1_无标签博文/1.html"
+                    }
+                ]
+            },
+            {
+                "title": "2_无标签博文",
+                "total_pages": 8,
+                "pages": [
+                    {
+                        "title": "1",
+                        "path": "./2_无标签博文/1.html"
+                    },
+                    {
+                        "title": "2",
+                        "path": "./2_无标签博文/2.html"
+                    },
+                    {
+                        "title": "3",
+                        "path": "./2_无标签博文/3.html"
+                    },
+                    {
+                        "title": "4",
+                        "path": "./2_无标签博文/4.html"
+                    },
+                    {
+                        "title": "5",
+                        "path": "./2_无标签博文/5.html"
+                    },
+                    {
+                        "title": "6",
+                        "path": "./2_无标签博文/6.html"
+                    },
+                    {
+                        "title": "7",
+                        "path": "./2_无标签博文/7.html"
+                    },
+                    {
+                        "title": "8",
+                        "path": "./2_无标签博文/8.html"
+                    }
+                ]
+            }
+        ]
+    }
+];
